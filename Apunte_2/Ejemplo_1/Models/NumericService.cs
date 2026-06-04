e703991ec32c76f60c744a1b4536fa9339cf55d3
molarovega <molarofrancisco@hotmail.com>
2026-06-04 18:14:13 -0300

    declaracion clase

---
+++ Apunte_2/Ejemplo_1/Models/NumericService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Ejemplo_1.Models
+{
+    internal class NumericService
+    {
+        #region atributos
+        int[] LUs;
+        string[] Nombres;
+        double[] Notas;
+        int Contador;
+        #endregion
+
+        public NumericService() //constructor
+        {
+            LUs = new int[0];
+            Nombres = new string[0];
+            Notas = new double[0];
+            Contador = 0;
+        }
+
+        #region metodos
+        public void RegistrarAlumno(int lu, string nombre, double nota) 
+        {
+
+        }
+        public int VerContador() 
+        {
+            return 0;
+        }
+        public string VerAlumno(int idx) 
+        {
+            return "";
+        }
+        public int BuscarPorLUSecuencial(int lu) 
+        {
+            return 0;
+        }
+        public int BuscarPorLUBinario(int lu)
+        {
+            return 0;
+        }
+        public void OrdenarPorLUBurbuja() 
+        {
+
+        }
+        public void OrdenarPorQuickSort()
+        {
+
+        }
+
+        #endregion
+
+
+
+    }
+}
